309c1597559aef647ea36dc2249acc1310c33dbd
Frank van Beem <frankvanbeem@voormedia.com>
2018-09-27 14:27:40 +0200

    debug

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 CURRENT_TAG=`git describe --tags --exact-match $TRAVIS_COMMIT 2> /dev/null`
 # if [ "$TRAVIS_BRANCH" = "$CURRENT_TAG" ] && [ "$INTEGRATION_TESTS" != "true" ]
 # then
-mvn clean compile org.apache.felix:maven-bundle-plugin:bundle deploy -Dmaven.test.skip=true -P
-    #release --settings settings.xml
-# fi
+mvn clean compile org.apache.felix:maven-bundle-plugin:bundle deploy -Dmaven.test.skip=true
+# -P release --settings settings.xml
+#fi
+stat --printf="%s" "target/tinify-1.6.1.jar"
