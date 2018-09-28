337db2c83efb47068da0ffdf429f4b83817b8129
Frank van Beem <frankvanbeem@voormedia.com>
2018-09-28 15:02:42 +0200

    validate again2

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 CURRENT_TAG=`git describe --tags --exact-match $TRAVIS_COMMIT 2> /dev/null`
 
-if [ "$TRAVIS_BRANCH" = "$CURRENT_TAG" ] && [ "$DEPLOY_ON_SUCCESS" = "true" ]
+# [ "$TRAVIS_BRANCH" = "$CURRENT_TAG" ] &&
+if [ "$DEPLOY_ON_SUCCESS" = "true" ]
 then
   mvn clean compile org.apache.felix:maven-bundle-plugin:bundle deploy -Dmaven.test.skip=true -P release --settings settings.xml
 fi
