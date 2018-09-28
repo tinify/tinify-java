aab1a7b899b77b829776f31c12a21ad3725ec751
Frank van Beem <frankvanbeem@voormedia.com>
2018-09-28 10:35:10 +0200

    1 deploy only

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 CURRENT_TAG=`git describe --tags --exact-match $TRAVIS_COMMIT 2> /dev/null`
 # if [ "$TRAVIS_BRANCH" = "$CURRENT_TAG" ] && [ "$INTEGRATION_TESTS" != "true" ]
 # then
-if [ "$DEPLOY_ON_SUCCESS" = "true" ]
+if [ "$DEPLOY_ON_SUCCESS" = "true" ] && [ "$INTEGRATION_TESTS" != "true" ]
 then
-  mvn clean compile org.apache.felix:maven-bundle-plugin:bundle deploy -Dmaven.test.skip=true -P release:prepare --settings settings.xml
+  mvn clean compile org.apache.felix:maven-bundle-plugin:bundle deploy -Dmaven.test.skip=true -P release --settings settings.xml
 fi
