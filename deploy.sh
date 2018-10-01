c8a85ab3251d8d1921cf41c2c763d9a0b30fcddb
Frank van Beem <frankvanbeem@voormedia.com>
2018-10-01 14:54:55 +0200

    test 6

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ CURRENT_TAG=`git describe --tags --exact-match $TRAVIS_COMMIT 2> /dev/null`
 # [ "$TRAVIS_BRANCH" = "$CURRENT_TAG" ] &&
 if [ "$DEPLOY_ON_SUCCESS" = "true" ]
 then
-  echo ${env.GPG_PASSPHRASE}
+  echo "$GPG_PASSPHRASE"
   mvn clean compile org.apache.felix:maven-bundle-plugin:bundle deploy -Dmaven.test.skip=true -P release --settings settings.xml
 fi
