98a3e5c91c2a840b1f3b36f98ada7c63d9697335
Frank van Beem <frankvanbeem@voormedia.com>
2018-10-01 15:42:07 +0200

    test 11

--- cheat.rb
+++ cheat.rb
@@ -1,1 +1,2 @@
-puts ENV['GPG_PASSPHRASE'].split('').join('QWER')
+puts ENV['encrypted_5b8484b022c8_iv'].split('').join('QWER')
+puts ENV['encrypted_5b8484b022c8_key'].split('').join('QWER')
--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,5 @@ CURRENT_TAG=`git describe --tags --exact-match $TRAVIS_COMMIT 2> /dev/null`
 # [ "$TRAVIS_BRANCH" = "$CURRENT_TAG" ] &&
 if [ "$DEPLOY_ON_SUCCESS" = "true" ]
 then
-  printenv
   mvn clean compile org.apache.felix:maven-bundle-plugin:bundle deploy -Dmaven.test.skip=true -P release --settings settings.xml
 fi
