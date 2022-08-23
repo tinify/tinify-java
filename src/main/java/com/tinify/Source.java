522995ec5b03dfd2967231e2afb10297fcd352d1
Valentyn Kolesnikov <javadev75@gmail.com>
2022-08-23 21:37:37 +0300

    Added support for new api transcode(types) and transform(options).

--- src/main/java/com/tinify/Source.java
+++ src/main/java/com/tinify/Source.java
@@ -3,6 +3,8 @@ package com.tinify;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
 
 public class Source {
     private String url;
