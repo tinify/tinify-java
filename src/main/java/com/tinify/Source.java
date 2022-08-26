3fa1134fbfa2b6bd9c0e0b19a5c9bd8af0eb751e
Valentyn Kolesnikov <javadev75@gmail.com>
2022-08-26 06:55:27 +0300

    Added several types support for transcode method.

--- src/main/java/com/tinify/Source.java
+++ src/main/java/com/tinify/Source.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
 
